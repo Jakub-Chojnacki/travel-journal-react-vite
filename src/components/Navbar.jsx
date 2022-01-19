2ca696acbe614969b45aab0818c429ac430f0567
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2022-01-19 22:22:54 +0100

    deleted useless files and made my files structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+
+export default function Navbar(){
+    return (
+        <div className="navbar">
+            <img src="./images/logo.svg" alt="" />
+            <h3>my travel journal.</h3>
+        </div>
+    )
+}
